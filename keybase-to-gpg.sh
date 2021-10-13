b13d406b7cb0a988001ae707f287a1a8c2cd30b2
Clayton Hynfield <clayton@hynfield.org>
2021-10-13 05:08:21 +0000

    Add initial script to configure GPG and Git signing from Keybase

---
+++ keybase-to-gpg.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+export GPG_TTY=$(tty)
+echo "hostname: $(hostname)"
+keybase login
+keybase pgp export | gpg --import
+keybase pgp export --secret | gpg --import --allow-secret-key-import
+GPG_SECRET_KEYID=$(gpg --list-secret-keys  --keyid-format SHORT | awk '/^sec/{sub(".*/","",$2); print$2}')
+echo "default-key:0:\"$GPG_SECRET_KEYID\"" | gpgconf --change-options gpg
+echo test | gpg --clearsign
+keybase deprovision
+
+git config --global user.signingkey $GPG_SECRET_KEYID
+git config --global commit.gpgsign true
